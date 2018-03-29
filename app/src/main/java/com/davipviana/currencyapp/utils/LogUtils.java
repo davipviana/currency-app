1eddce92fee2828eb9c3d1bbe8052f7423a055b4
Davi Viana <davipviana@gmail.com>
2018-03-29 16:17:57 -0300

    Finalizada classe de log.

--- app/src/main/java/com/davipviana/currencyapp/utils/LogUtils.java
+++ app/src/main/java/com/davipviana/currencyapp/utils/LogUtils.java
@@ -1,5 +1,10 @@
 package com.davipviana.currencyapp.utils;
 
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class LogUtils {
 
     private static StringBuffer stringBuffer = new StringBuffer();
@@ -11,6 +16,36 @@ public class LogUtils {
     private static LogListener logListener;
 
     public static void log(String tag, String message) {
+        Log.d(tag, message);
+        StringBuilder stb = new StringBuilder();
+        String date = formatDate(Calendar.getInstance());
+        stb.append(date);
+        stb.append(" ");
+        stb.append(tag);
+        stb.append(" ");
+        stb.append(message);
+        stb.append("\n\n");
+        stringBuffer.insert(0, stb.toString());
+        printLogs();
+    }
 
+    private static String formatDate(Calendar calendar) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd hh:mm:ss");
+        return simpleDateFormat.format(calendar.getTime());
+    }
+
+    private static void printLogs() {
+        if(logListener != null) {
+            logListener.onLogged(stringBuffer);
+        }
+    }
+
+    public static void clearLogs() {
+        stringBuffer = new StringBuffer();
+        printLogs();
+    }
+
+    public static void setLogListener(LogListener logListener) {
+        logListener = logListener;
     }
 }
